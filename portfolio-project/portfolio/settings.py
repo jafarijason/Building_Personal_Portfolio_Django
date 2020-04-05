b6a1052c863790dbea0a15cab398f2a80fa47978
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-04-04 22:04:45 -0400

    chane files

--- portfolio-project/portfolio/settings.py
+++ portfolio-project/portfolio/settings.py
@@ -74,6 +74,8 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
+#change data base to postgres
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
